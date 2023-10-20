7c584c0bc7b1447680232dd8edc66e8ca44f4bec
Adrian Rybka <adrian_rybka1989@wp.pl>
2023-10-20 22:13:20 +0200

    add player dialog

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -23,9 +23,32 @@ sap.ui.define(
         if (!this.addDialog)
           this.addDialog = await Fragment.load({
             name: "players.view.fragment.Add",
+            controller: this,
           });
         this.getView().addDependent(this.addDialog);
+        const oBindingContext = this.getView()
+          .getModel()
+          .createEntry("/AttributesSet");
+        this.addDialog.setBindingContext(oBindingContext);
         this.addDialog.open();
+      },
+      handleEditToggled: function (event) {
+        if (event.getParameter("editable")) return;
+        const payload = event.getSource().getBindingContext().getObject();
+        const model = this.getView().getModel();
+        const fields = event.getSource().getSmartFields();
+        fields.forEach((field) => {
+          const path = field.getBinding("value").getPath();
+          payload[path] = field.getValue();
+        });
+        model.create("/AttributesSet", payload, {
+          success: (res) => {
+            console.log(res);
+          },
+          error: (err) => {
+            sap.m.MessageBox.error(err);
+          },
+        });
       },
     });
   }
